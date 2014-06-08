9d362ea37da74517b0e1155186a0b56720aa8434
Tim Sneed <trsneed@gmail.com>
2014-06-07 21:25:19 -0500

    Windows Phone Added to project :thumbsup:

---
+++ FootballTournament2014.WindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FootballTournament2014.WindowsPhone.Resources;
+
+namespace FootballTournament2014.WindowsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ FootballTournament2014.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FootballTournament2014.WindowsPhone.Resources;
+using Xamarin;
+using Xamarin.Forms;
+
+namespace FootballTournament2014.WindowsPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            FormsMaps.Init();
+
+            Content = FootballTournament2014.App.RootPage.ConvertPageToUIElement(this);
+        }
+    }
+}
